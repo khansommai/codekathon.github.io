286fed0ccf5e00ab29c2790a5fb6aeba3ad7db21
khansommai <55424925+khansommai@users.noreply.github.com>
2022-06-03 23:30:56 +0700

    Update line.py

--- py_script/line.py
+++ py_script/line.py
@@ -2,7 +2,7 @@
 import requests
 # LINE notify
 url = 'https://notify-api.line.me/api/notify'
-token = 'vsz5C3DmtRC3L8dWQ0Y2TGDRy3HB13DSj4ajwUSNhPF'
+token = ''
 headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
 msg = 'ทดสอบการแจ้งเตือน'
 r = requests.post(url, headers=headers, data = {'message':msg})
